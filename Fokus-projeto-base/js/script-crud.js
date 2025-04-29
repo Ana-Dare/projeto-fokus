0da0fd3e9ea8f5a573378fef26b7ab02e7b5f82d
Ana Daré <analauradare467@gmail.com>
2025-04-28 22:43:04 -0300

    Desselecionando tarefas

--- Fokus-projeto-base/js/script-crud.js
+++ Fokus-projeto-base/js/script-crud.js
@@ -7,9 +7,12 @@ const textarea = document.querySelector('.app__form-textarea')
 
 const ulTarefas = document.querySelector('.app__section-task-list')
 const tarefas = JSON.parse(localStorage.getItem('tarefas')) || []
+let tarefaSelecionada = null
 const paragrafoDescricaoTarefa = document.querySelector('.app__section-active-task-description')
 
 const btnCancelarTarefa = document.querySelector('.app__form-footer__button--cancel')
+
+const classeAtiva = 'app__section-task-list-item-active'
 
 function atualizarTarefas () {
     localStorage.setItem('tarefas' , JSON.stringify(tarefas)) //atualiza a descrição da tarefa no localStore
@@ -77,10 +80,23 @@ function criarElementoTarefa(tarefa) {
     li.append(botaoEditar)
 
     li.onclick = () => {
+        document.querySelectorAll('.app__section-task-list-item-active')
+                .forEach(elemento => {
+                        elemento.classList.remove('app__section-task-list-item-active')
+    })
+        
+
+        if (tarefaSelecionada == tarefa) {
+            paragrafoDescricaoTarefa.textContent = ''
+            tarefaSelecionada = null
+            liTarefaSelecionada = null
+            return
+}
+
+        tarefaSelecionada = tarefa
         paragrafoDescricaoTarefa.textContent = tarefa.descricao
         li.classList.add('app__section-task-list-item-active')
     }
-
     return li
 }
 
@@ -91,5 +107,5 @@ tarefas.forEach(tarefa => {
 
 });
 
-btnCancelarTarefa.addEventListener('click', cancelarTarefa)
 
+btnCancelarTarefa.addEventListener('click', cancelarTarefa)
